9bec87092e1396597dcae61c42d128e2b8afad00
TALHA <molabuxr@gmail.com>
2021-08-05 06:19:32 -0700

    Andorid Studio 6th Project

--- app/src/main/java/com/example/fwms/Splashscreen.java
+++ app/src/main/java/com/example/fwms/Splashscreen.java
@@ -2,13 +2,36 @@ package com.example.fwms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splashscreen extends AppCompatActivity {
 
+    Thread timer;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        timer = new Thread(){
+            public void run(){
+                try {
+                    synchronized (this){
+                        wait(5000);
+                    }
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent inte = new Intent(Splashscreen.this,Login.class);
+                    startActivity(inte);
+                    finish();
+                }
+            }
+        };
+
+
     }
 }
